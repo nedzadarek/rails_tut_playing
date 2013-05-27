893f03334c116b66c5fc72b791c118035a751b2f
Darek Nedza <nedzadarek@gmail.com>
2013-05-27 17:52:04 +0200

    trying localization, languages: en&pl, translated only /teas; based mostly on railscast.com 138 movie + rest is my tries

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale, :set_user_language 
+  def set_user_language
+    I18n.locale = $locale#request.params["locale"] || "pl"
+    
+  end
+  
+  
+  
+  def set_locale
+    $locale = request.params["locale"] if request.params["locale"]
+  end
+  protected
+  $locale = "en"
+  
 end
